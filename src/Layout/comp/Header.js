0a2e05a2b40d16dc02d960d9edc6d933c4a32944
TroyPalmer93 <78763244+TroyPalmer93@users.noreply.github.com>
2021-05-09 20:36:21 -0700

    Add files via upload

---
+++ src/Layout/comp/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+function Header() {
+  return (
+    <header className="jumbotron bg-dark">
+      <div className="container text-white">
+        <h1 className="display-4">Full Of Think</h1>
+        <p className="lead">Find Your Mind</p>
+      </div>
+    </header>
+  );
+}
+
+export default Header;
